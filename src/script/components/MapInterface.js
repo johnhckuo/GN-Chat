16ab0b81d949a2828b5d2014191d74001f7ed34d
Calhoun5 <johnhckuo@gmail.com>
2018-03-12 16:44:27 +0800

    google map init

---
+++ src/script/components/MapInterface.js
@@ -0,0 +1,99 @@
+import React from "react"
+import { connect } from "react-redux"
+
+import * as Map from "../actions/Map"
+
+export default class MapInterface extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      zoom: 13,
+      lat: -33.8688,
+      lng: 151.2195,
+      maptype: 'roadmap',
+      place_formatted: '',
+      place_id: '',
+      place_location: '',
+    };
+  }
+
+  componentDidMount() {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: this.state.lat, lng: this.state.lng},
+      zoom: 13,
+      mapTypeId: 'roadmap',
+    });
+    map.addListener('zoom_changed', () => {
+      this.setState({
+        zoom: map.getZoom(),
+      });
+    });
+
+    map.addListener('drag', (e) => {
+      this.setState({
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng()
+      });
+    });
+
+    map.addListener('maptypeid_changed', () => {
+      this.setState({
+        maptype: map.getMapTypeId(),
+      });
+    });
+
+
+    let marker = new window.google.maps.Marker({
+      map: map,
+      position: {lat: this.state.lat, lng: this.state.lng},
+    });
+
+    // initialize the autocomplete functionality using the #pac-input input box
+    let inputNode = document.getElementById('pac-input');
+    map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(inputNode);
+    let autoComplete = new window.google.maps.places.Autocomplete(inputNode);
+
+    autoComplete.addListener('place_changed', () => {
+      let place = autoComplete.getPlace();
+      let location = place.geometry.location;
+
+      this.setState({
+        place_formatted: place.formatted_address,
+        place_id: place.place_id,
+        place_location: location.toString(),
+      });
+
+      // bring the selected place in view on the map
+      map.fitBounds(place.geometry.viewport);
+      map.setCenter(location);
+
+      marker.setPlace({
+        placeId: place.place_id,
+        location: location,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="mapContainer">
+        <div id='mapState'>          
+            Zoom level: {this.state.zoom}<br />
+            Map type: {this.state.maptype}<br />
+            Latitude: {this.state.lat}<br />
+            Longtitude: {this.state.lng}<br />
+            Place: {this.state.place_formatted}<br />
+            Place ID: {this.state.place_id}<br />
+            Location: {this.state.place_location}<br />
+        </div>
+        <div id='pac-container'>
+          <input id='pac-input' type='text' placeholder='Enter a location' />
+        </div>
+        <div id='map' />
+      </div>
+    ); 
+  } 
+}
+
+
--- src/script/main.js
+++ src/script/main.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 
 import Chatroom from "./components/Chatroom"
+import MapInterface from "./components/MapInterface"
+
 import store from "./store"
 import "../style/reset.scss"
 import "../style/chatroom.scss"
+import "../style/map.scss"
 import "../style/main.scss"
 
 const app = document.getElementById('app')
 
 ReactDOM.render(<Provider store={store}>
-  <Chatroom />
+   <MapInterface />
 </Provider>, app);

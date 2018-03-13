78826b0ffad086eb270d913bb6ad5a4fc1b28c7c
Calhoun5 <johnhckuo@gmail.com>
2018-03-13 11:37:23 +0800

    map location query issue fixed

--- src/script/components/MapInterface.js
+++ src/script/components/MapInterface.js
@@ -51,29 +51,70 @@ export default class MapInterface extends React.Component {
 
     // initialize the autocomplete functionality using the #pac-input input box
     let inputNode = document.getElementsByClassName('map__container__location__input')[0];
+    var searchBox = new google.maps.places.SearchBox(inputNode);
     map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(inputNode);
     let autoComplete = new window.google.maps.places.Autocomplete(inputNode);
+    
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
 
-    autoComplete.addListener('place_changed', () => {
-      let place = autoComplete.getPlace();
-      let location = place.geometry.location;
+    var markers = [];
+    searchBox.addListener('places_changed', () => {
+      var places = searchBox.getPlaces();
+      
+      if (places.length == 0 ){
+        return;
+      }
 
-      this.setState({
-        place_formatted: place.formatted_address,
-        place_id: place.place_id,
-        place_location: location.toString(),
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
       });
+      markers = [];
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach((place) => {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
 
-      // bring the selected place in view on the map
-      map.fitBounds(place.geometry.viewport);
-      map.setCenter(location);
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
 
-      marker.setPlace({
-        placeId: place.place_id,
-        location: location,
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+
+        let location = place.geometry.location;
+        this.setState({
+          place_formatted: inputNode.value,
+          place_id: place.place_id,
+          place_location: location.toString(),
+        });
+
       });
+      map.fitBounds(bounds);
     });
   }
+
 
   render() {
     return (
@@ -81,8 +122,8 @@ export default class MapInterface extends React.Component {
         <div className='map__container__state'>
             Zoom level: {this.state.zoom}<br />
             Map type: {this.state.maptype}<br />
-            Latitude: {this.state.lat}<br />
-            Longtitude: {this.state.lng}<br />
+            Latitude: {this.state.lat.toFixed(5)}<br />
+            Longtitude: {this.state.lng.toFixed(5)}<br />
             Place: {this.state.place_formatted}<br />
             Place ID: {this.state.place_id}<br />
             Location: {this.state.place_location}<br />
